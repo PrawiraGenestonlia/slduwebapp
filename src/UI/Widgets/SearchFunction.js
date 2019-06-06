bc255724a0978c5d9b414cfcf5a9c11dbcd729cc
Prawira <prawira.96@gmail.com>
2019-06-06 12:50:29 +0800

    SerchFunction fixed - wira

--- src/UI/Widgets/SearchFunction.js
+++ src/UI/Widgets/SearchFunction.js
@@ -1,85 +1,140 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import {Select} from 'antd';
+import { Select } from 'antd';
+import { throwStatement } from '@babel/types';
+import E7_EventTable from '../../datavisualisation/components/E7_EventTable';
 
 /*This is a search function whereby users can select how they want to search. For example, student name/matric number.*/
 
-const {Option} = Select;
-const SearchType = ['EventName','MatriculationNumber','StudentName'];
+const { Option } = Select;
+const DataVizColors = ['#8889DD', '#9597E4', '#8DC77B', '#A5D297', '#E2CF45', '#F8C12D'];
+const SearchType = ['EventName', 'MatriculationNumber', 'StudentName'];
 
-//I need to convert this variable into a state 
-var SearchData = {
+export default class SearchFunction extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refresh: false,
+      searchfunction: SearchType[0],
+      allData: {
         EventName: [],
         MatriculationNumber: [],
         StudentName: []
-};
-
-export default class SearchFunction extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            searchfunction: SearchData[SearchType[0]],
-            data: SearchData[SearchType[0]],
-        }
-    };
-    
-    handleSearchFunctionChange = value =>{
-
-    this.setState({
-            searchfunction: SearchData[value],
-            data: SearchData[SearchType[value][0]]
-        });
-    };
-
-    handSearchDataChange = value => {
-        this.setState({
-            data: value,
-        });
-    };
-
-    componentDidMount(){
-        axios.get(`localhost:8080/api/events`)
-        .then(response => 
-            this.setState({EventName: response.data})
-        ).catch(error => console.log(error));
-    
-        axios.get(`http://localhost:8080/api/students/?matricnumber`)
-        .then(response =>
-            this.setState({MatriculationNumber: response.data})
-        ).catch(error => console.log(error));
-        
-        axios.get(`http://localhost:8080/api/students/?studentname`)
-        .then(response =>
-            this.setState({StudentName: response.data})
-        ).catch(error => console.log(error));
-
-    };
-    
-    render(){
-        const{searchfunction} = this.state;
-        return(
-            <div>
-                <Select
-                    defaultValue={SearchType[0]}
-                    style={{ width:120}}
-                    onChange={this.handleSearchFunctionChange}
-                >
-                    {SearchType.map(type=> (
-                        <Option key={type}>{type}</Option>
-                    ))}
-                </Select>
-
-                <Select
-                    value={this.state.data}
-                    style={{ width:120}}
-                    onChange={this.handleSearchDataChange}
-                >
-                    {searchfunction.map(data=> (
-                        <Option key={data}>{data}</Option>
-                    ))}
-                </Select>
-            </div>
-        );
+      },
+      data: [],
+      tableData: [],
     }
+  };
+
+  handleSearchFunctionChange = value => {
+    this.setState({
+      searchfunction: value,
+    });
+    this.handleUpdateData();
+    this.handlePickData(value);
+  };
+
+  handleSeachDataChange = value => {
+    if (this.state.searchfunction == "EventName") {
+      axios.get(`http://localhost:8080/api/events/?eventname=${value}`)
+        .then(response => {
+          this.setState({ tableData: response.data });
+          console.log(response.data);
+        })
+        .catch(error => console.log(error));
+    }
+    else if (this.state.searchfunction == "MatriculationNumber") {
+      axios.get(`http://localhost:8080/api/students/?matricnumber=${value}`)
+        .then(response =>
+          this.setState({ tableData: response.data }))
+        .catch(error => console.log(error));
+    }
+    else if (this.state.searchfunction == "StudentName") {
+      axios.get(`http://localhost:8080/api/students/?studentname=${value}`)
+        .then(response =>
+          this.setState({ event: response.data }))
+        .catch(error => console.log(error));
+    }
+    console.log("tableData:", this.state.tableData);
+
+  };
+
+  handleUpdateData = () => {
+    let { allData } = this.state;
+    axios.get(`http://localhost:8080/api/events`)
+      .then(response =>
+        allData.EventName = response.data
+      ).catch(error => console.log(error));
+
+    axios.get(`http://localhost:8080/api/students/?matricnumber=''`)
+      .then(response =>
+        allData.MatriculationNumber = response.data
+      ).catch(error => console.log(error));
+
+    axios.get(`http://localhost:8080/api/students/?studentname=''`)
+      .then(response =>
+        allData.StudentName = response.data
+      ).catch(error => console.log(error));
+    this.setState({ allData: allData });
+  }
+
+  handlePickData = (value) => {
+    let { allData } = this.state;
+    let selectedData = [];
+    switch (value) {
+      case "EventName":
+        for (let i = 0; i < allData.EventName.length; i++)
+          selectedData.push(allData.EventName[i].TABLE_NAME);
+        break;
+      case "MatriculationNumber":
+        for (let i = 0; i < allData.MatriculationNumber.length; i++)
+          selectedData.push(allData.MatriculationNumber[i].TABLE_NAME); //change tablename to the correct value
+        break;
+      case "StudentName":
+        for (let i = 0; i < allData.StudentName.length; i++)
+          selectedData.push(allData.StudentName[i].TABLE_NAME); //change tablename to the correct value
+        break;
+      default:
+        break;
+    }
+    if (!selectedData.length)
+      selectedData = [];
+    this.setState({ data: selectedData });
+    setTimeout(() => {
+      console.log("data:", this.state.data);
+    }, 50)
+  }
+
+  componentDidMount() {
+    this.handleUpdateData();
+  };
+
+  render() {
+    return (
+      <div>
+        <Select
+          showSearch
+          placeholder="Select an option"
+          style={{ width: 120 }}
+          onChange={this.handleSearchFunctionChange} >
+          {SearchType.map(type => (
+            <Option key={type}>{type}</Option>
+          ))}
+        </Select>
+
+        <Select
+          showSearch
+          placeholder="Select a target"
+          style={{ width: 120 }}
+          onChange={this.handleSeachDataChange} >
+          {this.state.data.map(data => (
+            <Option key={data}>{data}</Option>
+          ))}
+        </Select>
+
+        {this.state.tableData.length ? <E7_EventTable data={this.state.tableData} shouldShow={true} colors={DataVizColors} /> : null}
+      </div>
+    );
+  }
 
 }
