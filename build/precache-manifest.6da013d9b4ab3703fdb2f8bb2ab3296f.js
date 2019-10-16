c281816b104982ac8fdf222be09a4d98decc421f
PrawiraGenestonlia <prawira.96@gmail.com>
2019-10-16 12:50:44 +0000

    Fast Commit

---
+++ build/precache-manifest.6da013d9b4ab3703fdb2f8bb2ab3296f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed661cb6436101155a02bd294359081c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1348ed2aef43d585943",
+    "url": "/static/css/2.51aa83c6.chunk.css"
+  },
+  {
+    "revision": "ae6ecb3997859835138c",
+    "url": "/static/css/main.ac7bddf5.chunk.css"
+  },
+  {
+    "revision": "e1348ed2aef43d585943",
+    "url": "/static/js/2.e0cd70cb.chunk.js"
+  },
+  {
+    "revision": "ae6ecb3997859835138c",
+    "url": "/static/js/main.a378582b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1cfe708293d19890ae73cb4f9d4ffca8",
+    "url": "/static/media/Logo.1cfe7082.png"
+  }
+]);
