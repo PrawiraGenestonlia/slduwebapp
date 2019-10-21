183eef1f6e4e1da63bd3c4245dbf40701bedeb83
Prawira Genestonlia <prawira.96@gmail.com>
2019-10-21 14:57:50 +0800

    from sldunoc

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6da013d9b4ab3703fdb2f8bb2ab3296f.js"
+  "/precache-manifest.04b99334630bf5326bdd352531f36269.js"
 );
 
 self.addEventListener('message', (event) => {
